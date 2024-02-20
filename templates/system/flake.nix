3ad2f56c8f3b0824676dfe778909707a42836461
Max Bolotin <maks.bolotin@gmail.com>
2024-02-20 16:53:06 +0100

    add templates

--- templates/system/flake.nix
+++ templates/system/flake.nix
@@ -4,12 +4,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # Reference Copper's dotfiles for configuration re-use
-    copper.url = "github:Cu3PO42/gleaming-glacier/next";
+    copper.url = "github:Cu3PO42/gleaming-glacier";
     # Replace the upstream's nixpkgs with our own, so we don't unnecessarily
     # duplicate dependencies.
     copper.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: inputs.copper.lib.mkGleamingFlake inputs ./. "maxdots" (flakeModules: {
+  outputs = inputs: inputs.copper.lib.mkGleamingFlake inputs ./. "myprefix" (flakeModules: {
+    # Your custom flake attrributes go here!
   });
 }
