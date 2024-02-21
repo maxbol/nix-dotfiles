d9cd98a4f54d2808a1907469de9a828c12e33684
Max Bolotin <maks.bolotin@gmail.com>
2024-02-21 11:04:05 +0100

    git feature

---
+++ features/home-manager/git.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }: {
+  home.packages = [ pkgs.gh ];
+
+  programs.git = {
+      enable = true;
+      package = pkgs.gitAndTools.gitFull;
+
+      userName = "Max Bolotin";
+      userEmail = "maks.bolotin@gmail.com";
+
+      aliases = {
+      adog = "log --all --decorate --oneline --graph";
+      };
+  };
+}
