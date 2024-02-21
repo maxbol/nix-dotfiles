93f013de5efbf92d5d52a800ea51cd9dfa38e973
Max Bolotin <maks.bolotin@gmail.com>
2024-02-21 16:29:01 +0100

    test

--- users/max@jockey.nix
+++ users/max@jockey.nix
@@ -2,42 +2,42 @@
   modules = [{
     copper.features = builtins.trace "sets features" [
       /* Common */
-      "catppuccin"
-      "chroma"
+      #"catppuccin"
+      #"chroma"
       "cli"
       "fish"
       "git"
-      "link-config"
-      "nvim"
-      "zsh"
+      #"link-config"
+      #"nvim"
+      #"zsh"
 
       /* NixOS specific */
-      "nixos/_1password"
-      "nixos/dunst"
-      "nixos/hyprland"
-      "nixos/rofi"
-      "nixos/swaylock"
-      "nixos/swim"
-      "nixos/waybar-hyprdots"
-      "nixos/waybar"
-      "nixos/wlogout"
+      #"nixos/_1password"
+      #"nixos/dunst"
+      #"nixos/hyprland"
+      #"nixos/rofi"
+      #"nixos/swaylock"
+      #"nixos/swim"
+      #"nixos/waybar-hyprdots"
+      #"nixos/waybar"
+      #"nixos/wlogout"
     ];
     copper.file.symlink.base = "/home/max/dotfiles";
     # copper.file.symlink.enable = true;
 
     maxdots.features = [
-      "azure"
-      "calendar"
-      "chrome"
-      "gaming"
-      "git"
-      "media"
-      "node"
-      "productivity"
-      "security"
-      "slack"
-      "utils"
-      "vscode"
+      #"azure"
+      #"calendar"
+      #"chrome"
+      #"gaming"
+      #"git"
+      #"media"
+      #"node"
+      #"productivity"
+      #"security"
+      #"slack"
+      #"utils"
+      #"vscode"
     ];
   }];
   system = "x86_64-linux";
