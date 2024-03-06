d516e013bc38d66ef191708faa56c380d4f1368b
Max Bolotin <maks.bolotin@gmail.com>
2024-03-06 17:12:16 +0100

    more tmux stuff

--- features/home-manager/cli/tmux.nix
+++ features/home-manager/cli/tmux.nix
@@ -12,6 +12,19 @@
         set -g @catppuccin_flavour 'mocha'
         set -g @catppuccin_window_tabs_enabled on
         set -g @catppuccin_date_time '%H:%M'
+
+        bind-key -n C-0 select-window -t :0
+        bind-key -n C-0 select-window -t :1
+        bind-key -n C-0 select-window -t :2
+        bind-key -n C-0 select-window -t :3
+        bind-key -n C-0 select-window -t :4
+        bind-key -n C-0 select-window -t :5
+        bind-key -n C-0 select-window -t :6
+        bind-key -n C-0 select-window -t :7
+        bind-key -n C-0 select-window -t :8
+        bind-key -n C-0 select-window -t :9
+
+        bind-key -n C-T create-window
         '';
       }
     ];
