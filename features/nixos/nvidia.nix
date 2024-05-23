2ac941b095a5909dd96886aa6348f8b1a59793bc
Max Bolotin <maks.bolotin@gmail.com>
2024-05-23 09:36:39 +0200

    nvidia beta drivers, seem to work nicely

--- features/nixos/nvidia.nix
+++ features/nixos/nvidia.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   # Nvidia drivers are unfree software, if we want to use them, we must allow unfree
   nixpkgs.config.allowUnfree = true;
 
@@ -14,7 +18,14 @@
       nvidiaSettings = true;
       forceFullCompositionPipeline = true;
       powerManagement.enable = true;
-      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "555.42.02";
+        sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+        sha256_aarch64 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+        openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+        settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+        persistencedSha256 = lib.fakeSha256;
+      };
     };
   };
 
@@ -24,4 +35,3 @@
     videoDrivers = ["nvidia"];
   };
 }
-
